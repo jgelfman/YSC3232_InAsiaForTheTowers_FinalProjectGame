7d484a1489942afc9f033ab03990346ad1fe598e
Jonathan G <gelfman.jonathan@u.yale-nus.edu.sg>
2020-11-04 16:01:05 +0800

    Added (hopefully) fixed small components for main draw

--- app/src/main/java/com/example/inasiaforthetowers/Background.java
+++ app/src/main/java/com/example/inasiaforthetowers/Background.java
@@ -6,12 +6,15 @@ import android.graphics.BitmapFactory;
 
 public class Background {
 
-    int x = 0, y = 0;
     Bitmap background;
 
+    //builds the background as a bitmap using the dimensions of the screen
     Background(int screenX, int screenY, Resources res) {
 
+        //borrows the background picture element
         background = BitmapFactory.decodeResource(res, R.drawable.backgroundskygarden);
+
+        //scales the background to fit the screen size
         background = Bitmap.createScaledBitmap(background, screenX, screenY, false);
 
     }
--- app/src/main/java/com/example/inasiaforthetowers/Platform.java
+++ app/src/main/java/com/example/inasiaforthetowers/Platform.java
@@ -10,40 +10,62 @@ import static com.example.inasiaforthetowers.View.screenRatioY;
 public class Platform {
 
     Bitmap platform;
-    int width, height, x, y, platformsInWindow;
+    int width, height, platcoordx, platcoordy, platformsInWindow;
+    public Character player;
+    int platlocy = 1;
+    public int index = 0;
+    public static int getIndex;
 
-    Platform (int screenX, int screenY, Resources res) {
 
+    //creates the platform image
+    public Platform (int screenX, int screenY, Character player, Resources res) {
+
+        //builds the platform as a bitmap
         platform = BitmapFactory.decodeResource(res, R.drawable.platformskygarden);
 
+        //scales based on the picture dimensions and screen ratio
         width = platform.getWidth();
         height = platform.getHeight();
 
-        width /= 5;
+        width /= 15;
         height /= 53;
 
         width *= (int) screenRatioX;
         height *= (int) screenRatioY;
 
+        this.platcoordy = screenY;
+        this.platcoordx = screenX;
+
+        //creates the bitmap using the scaled height/width
         platform = Bitmap.createScaledBitmap(platform, width, height, false);
 
-        x = screenX / 2;
-        y = (int) (64 * screenRatioY);
-
     }
 
-    Bitmap generatePlatform () {
+    public void Update () {
+        //Check if player lands on platform
+        if (player.charlocy > platlocy && player.charcoordy + player.height >= platcoordy && player.charcoordy + player.height <= platcoordy + height
+                && player.charcoordx + player.width / 2 >= platcoordx && player.charcoordx + player.width / 2 <= platcoordx + width) {
+            player.charcoordx = platlocy;
+            getIndex = index;
+            player.charcoordy = platcoordy - player.height + 2;
+            player.jump = false;
+            player.jumpdone = true;
+        } else if (player.charcoordy == platlocy && player.charcoordy + player.height >= platlocy && player.charcoordy + player.height <= platcoordy + height
+                && (player.charcoordx + player.width / 2 <= platcoordx || player.charcoordx + player.width / 2 >= platcoordx + width)) {
 
-        platformsInWindow = 3;
+            // Faster falling
+            player.charcoordy = platlocy + 4;
 
-        if (platformsInWindow <= 3) {
-            return platform;
-        } else {
-            return platform;
+            // Mark as falling
+            player.jump = false;
+            player.jumpdone = false;
         }
-
+        //the first space the player will press will make the game began
+        //if (input.keyIs(KeyEvent.VK_SPACE)) {
+        //    //make block move down
+        //    dy = stored_dy;
+        //}
+        //y += dy;
     }
-
-
 
 }
