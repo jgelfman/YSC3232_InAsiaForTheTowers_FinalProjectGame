376fd1e4f0086939b70463640d9ff04715b87c50
Chris Lis <contact@chrislis.net>
2020-11-05 17:40:15 +0800

    Add the physics logic in the character. Not sure why this wasnt in prev commit

--- app/src/main/java/com/example/inasiaforthetowers/entities/PlayableCharacter.java
+++ app/src/main/java/com/example/inasiaforthetowers/entities/PlayableCharacter.java
@@ -13,6 +13,15 @@ public class PlayableCharacter {
     public int width;
     public int height;
 
+    public float velocityX = 0; // px/s
+    public float velocityY = 0; // px/s
+
+    public static final float MAX_SPEED = 5; // px/s
+    public static final float TERMINAL_VELOCITY = 3; // px/s
+
+    public static final double GRAVITY = 0.05; // px/s^2
+    public static final double BREAKING = 0.1; // px/s^2
+
     public PlayableCharacter(Bitmap image, int sizeX, int sizeY){
 
         _image = Bitmap.createScaledBitmap(image, 400, 200, true);
@@ -23,12 +32,45 @@ public class PlayableCharacter {
         y = sizeY-200-20;;
         _sizeX = sizeX;
         _sizeY = sizeY;
-
     }
 
-    public void move(int dx, int dy) {
-        x += dx;
-        y += dy;
+    public void changeSpeed(float dx, float dy) {
+        if (dx > MAX_SPEED) {
+            velocityX = MAX_SPEED;
+        } else if (dx < -MAX_SPEED) {
+            velocityX = -MAX_SPEED;
+        } else {
+            velocityX = dx;
+        }
+
+        if (dy < 0) {
+            if (dy > MAX_SPEED) {
+                velocityY = MAX_SPEED;
+            } else {
+                velocityY = dy / 4;
+            }
+        }
+    }
+
+    public void move(float time) {
+        x += velocityX * time;
+        y += velocityY * time;
+        if (velocityX < 0) {
+            velocityX += BREAKING * time;
+            if (velocityX > 0) {
+                velocityX = 0;
+            }
+        } else {
+            velocityX -= BREAKING * time;
+            if (velocityX < 0) {
+                velocityX = 0;
+            }
+        }
+
+        velocityY += GRAVITY * time;
+        if (velocityY > TERMINAL_VELOCITY) {
+            velocityY = TERMINAL_VELOCITY;
+        }
     }
 
     public void draw(Canvas canvas){
